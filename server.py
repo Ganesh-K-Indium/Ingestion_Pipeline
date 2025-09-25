d2f2e55ba46b23a137c2cb8b0378ba7a40d074e6
GaneshK_Indium <ganesh.k@indium.tech>
2025-09-25 22:43:55 +0530

    update

--- server.py
+++ server.py
@@ -5,8 +5,20 @@ from fastapi.responses import StreamingResponse
 from manager import ManagerAgent
 from app_logger import log_stream
 
+
+
 app = FastAPI()
 agent = ManagerAgent()
+
+from fastapi.middleware.cors import CORSMiddleware
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],   # or restrict to ["http://localhost:3000"]
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 async def async_stream(gen):
